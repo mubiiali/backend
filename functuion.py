a0b3f08df97554e4016465397093cd29ebcf00e8
mubiiali <125971501+mubiiali@users.noreply.github.com>
2023-10-03 14:17:19 +0500

    Add files via upload

---
+++ functuion.py
@@ -0,0 +1,6 @@
+def calculate_tax(bill, tax_rate):
+    return(bill*tax_rate)/100.00
+
+print('total tax;',calculate_tax(175.00, 15))
+
+print('total tax;',calculate_tax(164.00, 15))
---
+++ methods.py
@@ -0,0 +1,6 @@
+pets ="cats & dogs"
+pets.index("&")
+
+pets.index("a")
+pets.index("dogs")
+print ("dragons" in pets)
