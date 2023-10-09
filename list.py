f2a8c185340895684e793017c8531c7f48cd7c34
mubiiali <muhammadmubashar1214@gmail.com>
2023-10-09 16:46:22 +0500

    tuple

---
+++ list.py
@@ -0,0 +1,10 @@
+fruits=["pineapple","banana","apple","melon"]
+# fruits.append=("kiwi")
+# print(fruits)
+
+
+# insert
+# fruits.insert(0,"orange")
+# fruits.remove("melon")
+print(fruits.pop(3))
+print(fruits)
