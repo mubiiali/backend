75926c126fa4f7d9b01e23903c342d881dce3ec2
mubiiali <muhammadmubashar1214@gmail.com>
2023-10-16 15:31:14 +0500

    chsnges

---
+++ .py
@@ -0,0 +1,2 @@
+multiples=[x*7 for x in range(1,11)]
+print(multiples)
--- methods.py
+++ methods.py
@@ -4,3 +4,8 @@ pets.index("&")
 pets.index("a")
 pets.index("dogs")
 print ("dragons" in pets)
+
+
+#list comprehension
+multiples = [x*7 for x in range(1,11)]
+print(multiples)
